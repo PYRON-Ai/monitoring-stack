c1ff234a97bf518cca7442968afa4c2cec475ddc
Andreh Torres <andreh.torres@hotmail.com>
2025-12-05 00:02:43 -0300

    feat: terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,9 +4,9 @@ terraform {
       s3 = "https://sgp1.digitaloceanspaces.com"
     }
 
-    bucket   = "pyron-monitor-stack-tfstate"
-    key      = "staging/terraform.tfstate"
-    region   = "sgp1"
+    bucket                      = "pyron-monitor-stack-tfstate"
+    key                         = "staging/terraform.tfstate"
+    region                      = "sgp1"
     force_path_style            = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
